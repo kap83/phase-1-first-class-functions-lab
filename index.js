56155b91378f20ebad134197d12ee0cde2d509e8
Karen Polanco <polanco.k@gmail.com>
2022-11-10 17:35:53 -0500

    commit tree 7e59a35a5ee5152edf61a31f9553cf81ae4db52c

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution in this file!
+
+const returnFirstTwoDrivers = function(array){
+    return array.slice(0,2)
+}
+
+const returnLastTwoDrivers = function(array){
+    return array.slice(-2)
+}
+//use negative symbol for counting backwards
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = (integer) => (function(fare) {return fare * integer})
+
+const fareDoubler = createFareMultiplier(2)
+const fareTripler = createFareMultiplier(3)
+
+const selectDifferentDrivers = (array, fn) => fn(array)
